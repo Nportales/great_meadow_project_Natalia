b324c8dd27461dd4b2843f4589ada394c53ed337
Natalia Portales <nataliaisabel3@gmail.com>
2025-05-07 08:16:11 -0400

    added Kate's 2024 veg data and her sumVegMMI function script

--- scripts/sumVegMMI(2013_2023_data).R
+++ scripts/sumVegMMI(2013_2023_data).R
@@ -27,107 +27,82 @@
 #' }
 #' @export
 
-#---------------------------------------------#
-####        Load Required Packages         ####
-#---------------------------------------------#
-
-library(tidyverse)
-library(dplyr)
-
-#-----------------------#
-####    Read Data    ####
-#-----------------------#
-
-#Reading in CSVs as a tibble
-
-species_by_strata <- read.csv("data/raw_data/Kate_2024_veg_data/species_by_strata.csv") %>%
-  as_tibble()
-
-species_list <- read.csv("data/raw_data/Kate_2024_veg_data/species_list.csv") %>%
-  as_tibble()
-
-locations <- read.csv("data/raw_data/Kate_2024_veg_data/locations.csv") %>%
-  as_tibble()
-
-visits <- read.csv("data/raw_data/Kate_2024_veg_data/visits.csv") %>%
-  as_tibble()
-
-
-sumVegMMI <- function(site = "all", panel = -1, years = 2013:2024,
+sumVegMMI <- function(site = "all", panel = NA, years = 2013:2023,
                       QAQC = FALSE){
-
+  
   #---- Error Handling ----
   # Make more general for Non-NETN sites
   env <- if(exists("VIEWS_RAM")){VIEWS_RAM} else {.GlobalEnv}
   site_list <- tryCatch(unique(get("locations", envir = env)$Code),
                         error = function(e){stop("The locations table was not found. Please import wetland RAM views.")})
-
+  
   site <- match.arg(site, c("all", site_list), several.ok = TRUE)
   site <- if(any(site == "all")){site_list} else {site}
-
-  stopifnot(class(panel) %in% c("numeric", "integer"), all(panel %in% c(1, 2, 3, 4, -1)))
+  
+  stopifnot(class(panel) %in% c("numeric", "integer", "logical"), all(panel %in% c(1, 2, 3, 4, -1, NA)))
   stopifnot(class(years) %in% c("numeric", "integer"), years >= 2013)
   stopifnot(class(QAQC) == "logical")
-
+  
   #---- Compile Data ----
-  spplist <- tryCatch(get("species_list", envir = env)[,c("Code", "Location_ID", "Visit_ID", "Panel", "Date", "Year", "Visit_Type",
-                                                              "limited_RAM", "CoC_ME_ACAD", "TSN", "Latin_Name")],
+  spplist <- tryCatch(get("species_list_tlu", envir = env)[,c("Code", "Location_ID", "Visit_ID", "Panel", "Date", "Year", "Visit_Type",
+                                                          "limited_RAM", "CoC_ME_ACAD", "TSN", "Latin_Name")],
                       error = function(e){stop("The tbl_species_list table was not found. Please import wetland RAM views.")}
-                      )
-  strata <- get("species_by_strata", envir = env)[,c("Code", "Location_ID", "Visit_ID", "Panel", "Date", "Year", "Visit_Type",
-                                                         "limited_RAM", "CoC_ME_ACAD", "TSN", "Latin_Name", "Percent_Cover")]
-
+  )
+  strata <- get("species_by_strata_tlu", envir = env)[,c("Code", "Location_ID", "Visit_ID", "Panel", "Date", "Year", "Visit_Type",
+                                                     "limited_RAM", "CoC_ME_ACAD", "TSN", "Latin_Name", "Percent_Cover")]
+  
   visit <- get("visits", envir = env)[,c("Code", "Location_ID", "Visit_ID", "Panel", "Date", "Year", "Visit_Type", "limited_RAM",
-                                             "Bryophyte_Cover", "Invasive_Cover")]
+                                         "Bryophyte_Cover", "Invasive_Cover")]
   loc <- get("locations", envir = env)[,c("Code", "Location_ID", "Panel", "xCoordinate", "yCoordinate", "UTM_Zone")]
-
+  
   covtol <- strata |>
     mutate(cov_tol = ifelse(CoC_ME_ACAD <= 4, Percent_Cover, 0)) |>
     group_by(Code, Location_ID, Visit_ID, Panel, Year, Visit_Type) |>
     summarize(sum_cov_tol = sum(cov_tol, na.rm = T),
               .groups = "drop")
-
+  
   meanC <- spplist |> group_by(Code, Location_ID, Visit_ID, Panel, Year, Visit_Type) |>
     summarize(meanC = mean(CoC_ME_ACAD, na.rm = T),
               .groups = "drop")
-
+  
   comb <- purrr::reduce(list(visit, meanC, covtol), left_join,
                         by = c("Code", "Location_ID", "Visit_ID", "Panel", "Year", "Visit_Type"))
-
+  
   vmmi_calc <- comb |>
     mutate(meanC_adj1 = ifelse(meanC < 3.015, 3.015, ifelse(meanC > 7.346, 7.346, meanC)),
            meanC_adj2 = ((meanC_adj1 - 3.015)/(7.346 - 3.015)) * 10,
-
+           
            covtol_adj1 = ifelse(sum_cov_tol < 0.386, 0, ifelse(sum_cov_tol > 136.645, 136.645, sum_cov_tol)),
            covtol_adj2 = ((((covtol_adj1 - 0.386)/(136.645 - 0.386))*10) - 10) * -1,
-
+           
            invcov_adj1 = ifelse(Invasive_Cover > 38.45, 38.45, Invasive_Cover),
            invcov_adj2 = ((((invcov_adj1/38.45) * 10) - 10))*-1,
-
+           
            bryo_adj1 = ifelse(Bryophyte_Cover > 98.48, 98.48, Bryophyte_Cover),
            bryo_adj2 = (bryo_adj1/98.48) * 10,
-
+           
            vmmi = (((meanC_adj2 + covtol_adj2 + invcov_adj2 + bryo_adj2) - 0.389)/(40 - 0.389)) * 100,
            vmmi_rating = ifelse(vmmi > 65.22746, "Good", ifelse(vmmi < 52.785, "Poor", "Fair"))
-           )
-
+    )
+  
   vmmi_site <- filter(vmmi_calc, Code %in% site)
   vmmi_year <- filter(vmmi_site, Year %in% years)
   vmmi_panel <- filter(vmmi_year, Panel %in% panel)
   vmmi_qaqc <- if(QAQC == FALSE){filter(vmmi_panel, Visit_Type == "VS")} else {vmmi_panel}
-
+  
   if(nrow(vmmi_qaqc) == 0){
     stop("Arguments returned a data frame with no records. Be sure you specified RAM years, and not EPA NWCA years.")}
-
+  
   vmmi_final <- left_join(vmmi_qaqc, loc, by = c("Code", "Location_ID", "Panel")) |>
     select(Code, Location_ID, Visit_ID, Panel, xCoordinate, yCoordinate, UTM_Zone, Date, Year, Visit_Type,
            limited_RAM, meanC, Bryophyte_Cover, Invasive_Cover, Cover_Tolerant = sum_cov_tol,
            vmmi, vmmi_rating)
-
+  
   return(vmmi_final)
-
-  }
+  
+}
 
 result <- sumVegMMI()
 print(result)
+
 
