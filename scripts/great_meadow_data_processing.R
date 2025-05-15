0ac304fc42fa56c0b3497e86f19b577f8ec6060f
Natalia Portales <nataliaisabel3@gmail.com>
2025-05-15 17:29:45 -0400

    worked on fixing tlu_Plant and species_by_strata latin name mismatching

--- scripts/great_meadow_data_processing.R
+++ scripts/great_meadow_data_processing.R
@@ -39,6 +39,9 @@ visits <- read.csv("data/raw_data/Glen_2015_2023_veg_data/visits.csv") %>%
 
 ## combine tlu_Plant to species.list by common columns
 
+#first find columns with missing information
+na_rows_list <- species_list_tlu %>% filter(is.na(TSN))
+
 # Define the key column
 key_col <- "Latin_Name"
 
@@ -64,27 +67,43 @@ setdiff(names(species_list), names(species_list_tlu))
 
 ## combine tlu_Plant to species_by_strata by common columns
 
+#first find columns with missing information
+na_rows <- species_by_strata_tlu %>% filter(is.na(TSN))
+
+#rename latin name species using casewhen
+species_by_strata_new <- species_by_strata %>% 
+
+mutate(
+  Latin_Name =
+    case_when(Latin_Name == "Frangula alnus" ~ "Rhamnus frangula",
+              Latin_Name == "Alnus incana ssp. rugosa" ~ "Alnus incana",
+              TRUE ~ Latin_Name))
+
 # Define the key column
 key_col <- "Latin_Name"
 
 # Step 1: Find common columns (excluding the key)
-common_cols_2 <- intersect(setdiff(names(species_by_strata), key_col), names(tlu_Plant))
+common_cols_2 <- intersect(setdiff(names(species_by_strata_new), key_col), names(tlu_Plant))
 
 # Step 2: Join only on key and shared columns
 tlu_Plant_sub_2 <- tlu_Plant %>% select(all_of(c(key_col, common_cols_2)))
 
 # Step 3: Join and update values
-species_by_strata_tlu <- species_by_strata %>%
+species_by_strata_tlu <- species_by_strata_new %>%
   left_join(tlu_Plant_sub_2, by = key_col, suffix = c("", "_new")) %>%
   mutate(across(all_of(common_cols_2), ~ coalesce(get(paste0(cur_column(), "_new")), .x))) %>%
   select(-ends_with("_new"))
 
 #check that the same columns were kept in dataset 1
+setdiff(names(species_by_strata_new), names(species_by_strata_tlu))
 
-setdiff(names(species_by_strata), names(species_by_strata_tlu))
+
+
 
 # Save outputs as CSV
 # write.csv(species_by_strata_tlu, "data/processed_data/species_by_strata_tlu.csv", row.names = FALSE)
+
+
 
 
 
