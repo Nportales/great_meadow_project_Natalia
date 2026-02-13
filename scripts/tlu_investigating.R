f43f006b2985e61badf1b73797b408cbfcd2e8db
Natalia Portales <116000684+Nportales@users.noreply.github.com>
2026-02-13 17:32:28 -0500

    investigating differences between tlu_Plant datasets and working on developing a TSN QA/QC check process

--- scripts/tlu_investigating.R
+++ scripts/tlu_investigating.R
@@ -41,7 +41,7 @@ AA_char_2015 <- read.csv("data/raw_data/vegetation_data/FOA_veg_data/old_raw_veg
 tlu_Plant <- read.csv("data/raw_data/vegetation_data/FOA_veg_data/old_raw_veg_data/tlu_Plant.csv") %>%
   as_tibble()
 
-## 2024 FOA/Glen veg data ##
+## 2024-new FOA/Glen veg data ##
 
 species_by_strata_new <- read.csv("data/raw_data/vegetation_data/FOA_veg_data/ACAD_Wetland_RAM_Data_20260129_public/species_by_strata.csv") %>%
   as_tibble()
@@ -64,6 +64,11 @@ RAM_stressors_new <- read.csv("data/raw_data/vegetation_data/FOA_veg_data/ACAD_W
 AA_char_new <- read.csv("data/raw_data/vegetation_data/FOA_veg_data/ACAD_Wetland_RAM_Data_20260129_public/AA_char.csv")
 
 tlu_Plant_new <- read.csv("data/raw_data/vegetation_data/FOA_veg_data/ACAD_Wetland_RAM_Data_20260129_public/tlu_Plant.csv")
+
+
+## NETN veg data tlu ##
+
+tlu_Plant_NETN <- read.csv("data/raw_data/vegetation_data/NETN_veg_data/NETN_Wetland_RAM_Data_20260129_public/tlu_Plant.csv")
 
 
 ## site data ##
@@ -94,7 +99,7 @@ tlu_latin_mismatch <- anti_join(
 #     "Salix sericea"
 #   ))
 
-
+# build a table to view latin name mismatches ----------------------------------
 tlu_latin_mismatch <- bind_rows(
   tlu_Plant_new %>%
     distinct(Latin_Name) %>%
@@ -125,6 +130,174 @@ latin_mismatch_rows <- bind_rows(new_only_rows, old_only_rows)
 search_tlu <- filter(tlu_Plant_new, Latin_Name == "Bidens")
 
 
+# build a table to view tsn mismatches -----------------------------------------
+mismatched_tsn <- bind_rows(
+  tlu_Plant_new %>% distinct(TSN) %>% mutate(source = "new"),
+  tlu_Plant %>% distinct(TSN) %>% mutate(source = "old")
+) %>%
+  group_by(TSN) %>%
+  filter(n() == 1) %>%
+  ungroup()
+
+TSN_mismatch_rows <- bind_rows(
+  tlu_Plant_new %>%
+    semi_join(mismatched_tsn, by = "TSN") %>%
+    mutate(source_dataset = "tlu_Plant_new"),
+  
+  tlu_Plant %>%
+    semi_join(mismatched_tsn, by = "TSN") %>%
+    mutate(source_dataset = "tlu_Plant")
+)
+
+## tsn mismatches function -----------------------------------------------------
+
+find_tsn_mismatches <- function(df_new,
+                                df_old,
+                                tsn_col = TSN,
+                                new_name = "new",
+                                old_name = "old",
+                                drop_invalid = TRUE) {
+  
+  tsn_col <- enquo(tsn_col)
+  
+  # # Optional TSN cleaning
+  # clean <- function(df) {
+  #   if (drop_invalid) {
+  #     df %>%
+  #       filter(!is.na(!!tsn_col) & !!tsn_col > 0)
+  #   } else {
+  #     df
+  #   }
+  # }
+  # 
+  # df_new <- clean(df_new)
+  # df_old <- clean(df_old)
+  
+  # Identify TSNs that occur in only one dataset
+  mismatched_tsn <- bind_rows(
+    df_new %>% distinct(!!tsn_col) %>% mutate(source = new_name),
+    df_old %>% distinct(!!tsn_col) %>% mutate(source = old_name)
+  ) %>%
+    group_by(!!tsn_col) %>%
+    filter(n() == 1) %>%
+    ungroup()
+  
+  # Pull full rows for those TSNs
+  bind_rows(
+    df_new %>%
+      semi_join(mismatched_tsn, by = rlang::as_name(tsn_col)) %>%
+      mutate(source_dataset = new_name),
+    
+    df_old %>%
+      semi_join(mismatched_tsn, by = rlang::as_name(tsn_col)) %>%
+      mutate(source_dataset = old_name)
+  )
+}
+
+FOA_TSN_mismatch_rows <- find_tsn_mismatches(
+  df_new = tlu_Plant_new,
+  df_old = tlu_Plant,
+  tsn_col = TSN,
+  new_name = "tlu_Plant_new",
+  old_name = "tlu_Plant_NETN"
+)
+
+NETN_TSN_mismatch_rows <- find_tsn_mismatches(
+  df_new = tlu_Plant_new,
+  df_old = tlu_Plant,
+  tsn_col = TSN,
+  new_name = "tlu_Plant_new",
+  old_name = "tlu_Plant_NETN"
+)
+
+
+# what tsns are only in one dataset and not the other
+tsn_origin <- bind_rows(
+  tlu_Plant_new %>%
+    distinct(TSN) %>%
+    mutate(origin_dataset = "tlu_Plant_new"),
+  
+  tlu_Plant_NETN %>%
+    distinct(TSN) %>%
+    mutate(origin_dataset = "tlu_Plant_NETN")
+) %>%
+  group_by(TSN) %>%
+  filter(n() == 1) %>%   # TSN appears in only one dataset
+  ungroup()
+
+tsn_mismatch_rows <- bind_rows(
+  tlu_Plant_new %>%
+    semi_join(tsn_origin, by = "TSN") %>%
+    mutate(origin_dataset = "tlu_Plant_new"),
+  
+  tlu_Plant_NETN %>%
+    semi_join(tsn_origin, by = "TSN") %>%
+    mutate(origin_dataset = "tlu_Plant_NETN")
+)
+
+
+# tsn function 
+find_tsn_origin_rows <- function(df_a,
+                                 df_b,
+                                 tsn_col = TSN,
+                                 label_a,
+                                 label_b,
+                                 drop_invalid = TRUE) {
+  
+  tsn_col <- rlang::enquo(tsn_col)
+  
+  # Optional TSN cleaning
+  if (drop_invalid) {
+    df_a <- df_a %>% filter(!is.na(!!tsn_col) & !!tsn_col > 0)
+    df_b <- df_b %>% filter(!is.na(!!tsn_col) & !!tsn_col > 0)
+  }
+  
+  # Step 1: Identify TSNs that occur in only one dataset
+  tsn_origin <- bind_rows(
+    df_a %>%
+      distinct(!!tsn_col) %>%
+      mutate(origin_dataset = label_a),
+    
+    df_b %>%
+      distinct(!!tsn_col) %>%
+      mutate(origin_dataset = label_b)
+  ) %>%
+    group_by(!!tsn_col) %>%
+    filter(n() == 1) %>%
+    ungroup()
+  
+  # Step 2: Pull full rows associated with those TSNs
+  tsn_rows <- bind_rows(
+    df_a %>%
+      semi_join(tsn_origin, by = rlang::as_name(tsn_col)) %>%
+      mutate(origin_dataset = label_a),
+    
+    df_b %>%
+      semi_join(tsn_origin, by = rlang::as_name(tsn_col)) %>%
+      mutate(origin_dataset = label_b)
+  )
+  
+  list(
+    tsn_origin = tsn_origin,
+    rows = tsn_rows
+  )
+}
+
+tsn_results <- find_tsn_origin_rows(
+  df_a = tlu_Plant_new,
+  df_b = tlu_Plant_NETN,
+  tsn_col = TSN,
+  label_a = "tlu_Plant_new",
+  label_b = "tlu_Plant_NETN"
+)
+
+tsn_results$tsn_origin   # TSN → which dataset it comes from
+tsn_results$rows         # full rows for those TSNs
+
+#search tlu for tsn
+search_tlu_new <- filter(tlu_Plant_new, TSN == "29062")
+search_tlu_NETN <- filter(tlu_Plant_NETN, TSN == "29062")
+search_tlu <- filter(tlu_Plant, TSN == "29062")
 
 ## Quality control check-point code ## -----------------------------------------
 
