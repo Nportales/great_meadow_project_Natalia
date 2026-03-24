3a813f0bf6a8fa4ef71f52d8b1c340cde658e952
Natalia Portales <116000684+Nportales@users.noreply.github.com>
2026-03-24 15:45:19 -0400

    new version of sumVegMMI function with updated thresholds made by Kate; added code for adding the SEN site; updates to file names

--- scripts/sumVegMMI.R
+++ scripts/sumVegMMI.R
@@ -40,31 +40,38 @@ library(dplyr)
 
 #Reading in CSVs as a tibble
 
-## 2015-new year Glen Veg data ##
+## 2012-new year merged FOA and NETN veg data ##
 
-species_by_strata <- read.csv("data/raw_data/vegetation_data/FOA_veg_data/FOA_veg_data_2025/species_by_strata_2015_2025.csv") %>%
+species_by_strata <- read.csv("data/raw_data/vegetation_data/all_veg_data/species_by_strata_all_2012_2025.csv") %>%
   as_tibble()
 
-species_list <- read.csv("data/raw_data/vegetation_data/FOA_veg_data/FOA_veg_data_2025/species_list_2015_2025.csv") %>%
+species_list <- read.csv("data/raw_data/vegetation_data/all_veg_data/species_list_all_2012_2025.csv") %>%
   as_tibble()
 
-locations <- read.csv("data/raw_data/vegetation_data/FOA_veg_data/FOA_veg_data_2025/locations.csv") %>%
+locations <- read.csv("data/raw_data/vegetation_data/all_veg_data/locations_all.csv") %>%
   as_tibble()
 
-visits <- read.csv("data/raw_data/vegetation_data/FOA_veg_data/FOA_veg_data_2025/visits_2015_2025.csv") %>%
+visits <- read.csv("data/raw_data/vegetation_data/all_veg_data/visits_all_2012_2025.csv") %>%
   as_tibble()
 
-RAM_stressors <- read.csv("data/raw_data/vegetation_data/FOA_veg_data/FOA_veg_data_2025/RAM_stressors_2015_2025.csv") %>%
+RAM_stressors <- read.csv("data/raw_data/vegetation_data/all_veg_data/RAM_stressors_all_2012_2025.csv") %>%
   as_tibble()
 
-vertical_complexity <- read.csv("data/raw_data/vegetation_data/FOA_veg_data/FOA_veg_data_2025/vertical_complexity_2015_2025.csv") %>%
+vertical_complexity <- read.csv("data/raw_data/vegetation_data/all_veg_data/vertical_complexity_all_2012_2025.csv") %>%
   as_tibble()
+
+## NETN SEN site data (already processed) ##
+
+VMMI_NETN_SEN <- read.csv("data/processed_data/vegetation_data/NETN_vegMMI_allsites_2011-2025.csv") %>%
+  as_tibble()
+
 
 #--------------------------#
 ####    Run Function    ####
 #--------------------------#
 
-sumVegMMI <- function(site = "all", panel = -1, years = 2015:2025,
+
+sumVegMMI <- function(site = "all", panel = c(-1, 1:4), years = 2012:format(Sys.Date(), "%Y"),
                       QAQC = FALSE){
   
   #---- Error Handling ----
@@ -76,8 +83,8 @@ sumVegMMI <- function(site = "all", panel = -1, years = 2015:2025,
   site <- match.arg(site, c("all", site_list), several.ok = TRUE)
   site <- if(any(site == "all")){site_list} else {site}
   
-  stopifnot(class(panel) %in% c("numeric", "integer"), all(panel %in% c(1, 2, 3, 4, -1)))
-  stopifnot(class(years) %in% c("numeric", "integer"), years >= 2015)
+  stopifnot(class(panel) %in% c("numeric", "integer"), panel %in% c(1, 2, 3, 4, -1))
+  stopifnot(class(years) %in% c("numeric", "integer"), years >= 2012)
   stopifnot(class(QAQC) == "logical")
   
   #---- Compile Data ----
@@ -90,7 +97,7 @@ sumVegMMI <- function(site = "all", panel = -1, years = 2015:2025,
   
   visit <- get("visits", envir = env)[,c("Code", "Location_ID", "Visit_ID", "Panel", "Date", "Year", "Visit_Type", "limited_RAM",
                                          "Bryophyte_Cover", "Invasive_Cover")]
-  loc <- get("locations", envir = env)[,c("Code", "Location_ID", "Panel", "xCoordinate", "yCoordinate", "UTM_Zone")]
+  loc <- get("locations", envir = env)[,c("Code", "Location_ID", "Panel", "xCoordinate", "yCoordinate", "Latitude", "Longitude", "UTM_Zone")]
   
   covtol <- strata |>
     mutate(cov_tol = ifelse(CoC_ME_ACAD <= 4, Percent_Cover, 0)) |>
@@ -106,20 +113,21 @@ sumVegMMI <- function(site = "all", panel = -1, years = 2015:2025,
                         by = c("Code", "Location_ID", "Visit_ID", "Panel", "Year", "Visit_Type"))
   
   vmmi_calc <- comb |>
-    mutate(meanC_adj1 = ifelse(meanC < 3.015, 3.015, ifelse(meanC > 7.346, 7.346, meanC)),
-           meanC_adj2 = ((meanC_adj1 - 3.015)/(7.346 - 3.015)) * 10,
+    mutate(meanC_adj1 = ifelse(meanC < 3.00087, 3.00087, ifelse(meanC > 7.060764, 7.060764, meanC)),
+           meanC_adj2 = ((meanC_adj1 - 3.00087)/(7.060764 - 3.00087)) * 10,
            
-           covtol_adj1 = ifelse(sum_cov_tol < 0.386, 0, ifelse(sum_cov_tol > 136.645, 136.645, sum_cov_tol)),
-           covtol_adj2 = ((((covtol_adj1 - 0.386)/(136.645 - 0.386))*10) - 10) * -1,
+           covtol_adj1 = ifelse(sum_cov_tol < 1.106, 0, ifelse(sum_cov_tol > 150.551, 150.551, sum_cov_tol)),
+           covtol_adj2 = ((((covtol_adj1 - 1.106)/(150.551 - 1.106))*10) - 10) * -1,
            
-           invcov_adj1 = ifelse(Invasive_Cover > 38.45, 38.45, Invasive_Cover),
-           invcov_adj2 = ((((invcov_adj1/38.45) * 10) - 10))*-1,
+           invcov_adj1 = ifelse(Invasive_Cover > 76.585, 76.585, Invasive_Cover),
+           invcov_adj2 = ((((invcov_adj1/76.585) * 10) - 10))*-1,
            
-           bryo_adj1 = ifelse(Bryophyte_Cover > 98.48, 98.48, Bryophyte_Cover),
-           bryo_adj2 = (bryo_adj1/98.48) * 10,
+           bryo_adj1 = ifelse(Bryophyte_Cover > 98.98, 98.98, Bryophyte_Cover),
+           bryo_adj2 = (bryo_adj1/98.98) * 10,
            
-           vmmi = (((meanC_adj2 + covtol_adj2 + invcov_adj2 + bryo_adj2) - 0.389)/(40 - 0.389)) * 100,
-           vmmi_rating = ifelse(vmmi > 65.22746, "Good", ifelse(vmmi < 52.785, "Poor", "Fair"))
+           vmmi = (((meanC_adj2 + covtol_adj2 + invcov_adj2 + bryo_adj2) - 0.2667)/(40 - 0.2667)) * 100,
+           vmmi_rating_orig = ifelse(vmmi > 65.22746, "Good", ifelse(vmmi < 52.785, "Poor", "Fair")),
+           vmmi_rating = ifelse(vmmi > 60.94853, "Good", ifelse(vmmi < 41.48136, "Poor", "Fair"))
     )
   
   vmmi_site <- filter(vmmi_calc, Code %in% site)
@@ -131,18 +139,124 @@ sumVegMMI <- function(site = "all", panel = -1, years = 2015:2025,
     stop("Arguments returned a data frame with no records. Be sure you specified RAM years, and not EPA NWCA years.")}
   
   vmmi_final <- left_join(vmmi_qaqc, loc, by = c("Code", "Location_ID", "Panel")) |>
-    select(Code, Location_ID, Visit_ID, Panel, xCoordinate, yCoordinate, UTM_Zone, Date, Year, Visit_Type,
+    select(Code, Location_ID, Visit_ID, Panel, xCoordinate, yCoordinate, Latitude, Longitude, UTM_Zone, Date, Year, Visit_Type,
            limited_RAM, meanC, Bryophyte_Cover, Invasive_Cover, Cover_Tolerant = sum_cov_tol,
-           vmmi, vmmi_rating)
+           vmmi, vmmi_rating, vmmi_rating_orig)
   
   return(vmmi_final)
   
 }
 
+# view output
 VMMI_result <- sumVegMMI()
 print(VMMI_result)
 
+
+## add SEN site veg data ##
+
+VMMI_result_updated <- bind_rows(
+  VMMI_result,
+  VMMI_NETN_SEN %>%
+    filter(local_id == "GILM" | str_detect(Code, "304")) %>%
+    select(any_of(names(VMMI_result)))
+) %>% 
+  mutate(
+    Latitude = case_when(
+      str_detect(Code, "304") ~ 44.36275,
+      TRUE ~ Latitude
+    ),
+    Longitude = case_when(
+      str_detect(Code, "304") ~ -68.27162,
+      TRUE ~ Longitude
+    )
+  )
+
 # Save outputs as CSV
-# write.csv(VMMI_result, "data/processed_data/vegetation_data/FOA_VMMI_2015_2025.csv", row.names = FALSE)
+# write.csv(VMMI_result_updated, "data/processed_data/vegetation_data/FOA_NETN_VMMI_2011_2025_20260324.csv", row.names = FALSE)
+
+
+
+## Graveyard ## ----------------------------------------------------------------
+# 
+# sumVegMMI <- function(site = "all", panel = -1, years = 2015:2025,
+#                       QAQC = FALSE){
+#   
+#   #---- Error Handling ----
+#   # Make more general for Non-NETN sites
+#   env <- if(exists("VIEWS_RAM")){VIEWS_RAM} else {.GlobalEnv}
+#   site_list <- tryCatch(unique(get("locations", envir = env)$Code),
+#                         error = function(e){stop("The locations table was not found. Please import wetland RAM views.")})
+#   
+#   site <- match.arg(site, c("all", site_list), several.ok = TRUE)
+#   site <- if(any(site == "all")){site_list} else {site}
+#   
+#   stopifnot(class(panel) %in% c("numeric", "integer"), all(panel %in% c(1, 2, 3, 4, -1)))
+#   stopifnot(class(years) %in% c("numeric", "integer"), years >= 2015)
+#   stopifnot(class(QAQC) == "logical")
+#   
+#   #---- Compile Data ----
+#   spplist <- tryCatch(get("species_list", envir = env)[,c("Code", "Location_ID", "Visit_ID", "Panel", "Date", "Year", "Visit_Type",
+#                                                           "limited_RAM", "CoC_ME_ACAD", "TSN", "Latin_Name")],
+#                       error = function(e){stop("The tbl_species_list table was not found. Please import wetland RAM views.")}
+#   )
+#   strata <- get("species_by_strata", envir = env)[,c("Code", "Location_ID", "Visit_ID", "Panel", "Date", "Year", "Visit_Type",
+#                                                      "limited_RAM", "CoC_ME_ACAD", "TSN", "Latin_Name", "Percent_Cover")]
+#   
+#   visit <- get("visits", envir = env)[,c("Code", "Location_ID", "Visit_ID", "Panel", "Date", "Year", "Visit_Type", "limited_RAM",
+#                                          "Bryophyte_Cover", "Invasive_Cover")]
+#   loc <- get("locations", envir = env)[,c("Code", "Location_ID", "Panel", "xCoordinate", "yCoordinate", "UTM_Zone")]
+#   
+#   covtol <- strata |>
+#     mutate(cov_tol = ifelse(CoC_ME_ACAD <= 4, Percent_Cover, 0)) |>
+#     group_by(Code, Location_ID, Visit_ID, Panel, Year, Visit_Type) |>
+#     summarize(sum_cov_tol = sum(cov_tol, na.rm = T),
+#               .groups = "drop")
+#   
+#   meanC <- spplist |> group_by(Code, Location_ID, Visit_ID, Panel, Year, Visit_Type) |>
+#     summarize(meanC = mean(CoC_ME_ACAD, na.rm = T),
+#               .groups = "drop")
+#   
+#   comb <- purrr::reduce(list(visit, meanC, covtol), left_join,
+#                         by = c("Code", "Location_ID", "Visit_ID", "Panel", "Year", "Visit_Type"))
+#   
+#   vmmi_calc <- comb |>
+#     mutate(meanC_adj1 = ifelse(meanC < 3.015, 3.015, ifelse(meanC > 7.346, 7.346, meanC)),
+#            meanC_adj2 = ((meanC_adj1 - 3.015)/(7.346 - 3.015)) * 10,
+#            
+#            covtol_adj1 = ifelse(sum_cov_tol < 0.386, 0, ifelse(sum_cov_tol > 136.645, 136.645, sum_cov_tol)),
+#            covtol_adj2 = ((((covtol_adj1 - 0.386)/(136.645 - 0.386))*10) - 10) * -1,
+#            
+#            invcov_adj1 = ifelse(Invasive_Cover > 38.45, 38.45, Invasive_Cover),
+#            invcov_adj2 = ((((invcov_adj1/38.45) * 10) - 10))*-1,
+#            
+#            bryo_adj1 = ifelse(Bryophyte_Cover > 98.48, 98.48, Bryophyte_Cover),
+#            bryo_adj2 = (bryo_adj1/98.48) * 10,
+#            
+#            vmmi = (((meanC_adj2 + covtol_adj2 + invcov_adj2 + bryo_adj2) - 0.389)/(40 - 0.389)) * 100,
+#            vmmi_rating = ifelse(vmmi > 65.22746, "Good", ifelse(vmmi < 52.785, "Poor", "Fair"))
+#     )
+#   
+#   vmmi_site <- filter(vmmi_calc, Code %in% site)
+#   vmmi_year <- filter(vmmi_site, Year %in% years)
+#   vmmi_panel <- filter(vmmi_year, Panel %in% panel)
+#   vmmi_qaqc <- if(QAQC == FALSE){filter(vmmi_panel, Visit_Type == "VS")} else {vmmi_panel}
+#   
+#   if(nrow(vmmi_qaqc) == 0){
+#     stop("Arguments returned a data frame with no records. Be sure you specified RAM years, and not EPA NWCA years.")}
+#   
+#   vmmi_final <- left_join(vmmi_qaqc, loc, by = c("Code", "Location_ID", "Panel")) |>
+#     select(Code, Location_ID, Visit_ID, Panel, xCoordinate, yCoordinate, UTM_Zone, Date, Year, Visit_Type,
+#            limited_RAM, meanC, Bryophyte_Cover, Invasive_Cover, Cover_Tolerant = sum_cov_tol,
+#            vmmi, vmmi_rating)
+#   
+#   return(vmmi_final)
+#   
+# }
+# 
+# VMMI_result <- sumVegMMI()
+# print(VMMI_result)
+# 
+# # Save outputs as CSV
+# # write.csv(VMMI_result, "data/processed_data/vegetation_data/FOA_VMMI_2015_2025_20260310.csv", row.names = FALSE)
 
 
